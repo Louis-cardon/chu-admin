95c6a4a44f0830610cac04a71999bfb395f087c9
Louis-alt <cardonlouis@orange.fr>
2024-02-03 01:28:40 +0100

    ajout page annoncement

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import AnnouncementsComponent from '@/components/announcementsComponent';
 import ChallengeComponent from '@/components/challengeComponent';
 import Navigation from '@/components/navigation';
 import StatisticsComponent from '@/components/statisticsComponent';
@@ -8,7 +9,6 @@ import React, { FC, useState } from 'react';
 const Home: FC = () => {
   const [activePage, setActivePage] = useState<string>('');
 
-  // Function to handle page change based on navigation
   const handlePageChange = (page: string) => {
     setActivePage(page);
   };
@@ -17,7 +17,7 @@ const Home: FC = () => {
     <div className="flex h-screen bg-white text-black p-4">
       <Navigation onNavigate={handlePageChange} />
       <div className="pl-4 flex-1">
-        <StatisticsComponent />
+        <AnnouncementsComponent />
       </div>
     </div>
   );
---
+++ src/components/announcementsComponent.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+const AnnouncementsComponent = () => {
+  const [announcements, setAnnouncements] = useState([
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+  ]);
+  const [newAnnouncement, setNewAnnouncement] = useState('');
+
+  const handleNewAnnouncementChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setNewAnnouncement(e.target.value);
+  };
+
+  const submitNewAnnouncement = () => {
+    if (newAnnouncement.trim() !== '') {
+      setAnnouncements([...announcements, newAnnouncement]);
+      setNewAnnouncement('');
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full p-6 bg-white rounded-3xl shadow-sm shadow-gray-300 border border-gray-100">
+      <h1 className="text-3xl font-semibold mb-4 text-center">MES ANNONCES</h1>
+      <div className="flex-grow overflow-auto mb-4 p-4 shadow-sm shadow-gray-300 border border-gray-100 rounded-3xl">
+        {announcements.map((announcement, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between p-2 my-2 shadow-sm shadow-gray-300 border border-gray-100 rounded-3xl"
+          >
+            <span className="flex-grow">{announcement}</span>
+            <button>
+              <FontAwesomeIcon icon={faTrashAlt} className="text-gray-200" />
+            </button>
+          </div>
+        ))}
+      </div>
+      <h2 className="text-3xl font-semibold mb-4 text-center">
+        AJOUTER UNE ANNONCE
+      </h2>
+      <div className="flex-grow p-4 border text-sm rounded-3xl shadow-sm shadow-gray-300 border-gray-100">
+        <textarea
+          className="w-full h-32 p-4 mb-4 bg-gray-100 rounded-lg"
+          value={newAnnouncement}
+          onChange={(e) => setNewAnnouncement(e.target.value)}
+          placeholder="Écrivez votre annonce ici..."
+        />
+        <button
+          onClick={submitNewAnnouncement}
+          className="w-full text-white bg-blue-500 hover:bg-blue-700 font-semibold py-2 px-4 rounded-lg flex justify-center items-center"
+        >
+          Valider
+          <FontAwesomeIcon icon={faCheck} className="ml-2" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AnnouncementsComponent;
